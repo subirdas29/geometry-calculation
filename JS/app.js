32094eb78d811175d7669452d494749f086ba678
subirdas29 <dasjoy104@gmail.com>
2023-10-05 01:58:31 +0600

    all added

--- JS/app.js
+++ JS/app.js
@@ -1,38 +1,63 @@
 
 
-// document.getElementById('calculate-traingle').addEventListener('click',function(){
-//     const input1 = document.getElementById('inb')
-//     const input2 = document.getElementById('inh')
-
-//     const updateTrih = document.getElementById('tri-h')
-//     const updateTrib = document.getElementById('tri-b')
-
-//     const updateTriangle = document.getElementById('triangle-value')
-
-
-//     updateTrib.innerText = input1.value
-//     updateTrih.innerText = input2.value
-
-//     const totalArea = 0.5 * input1.value * input2.value
-
-//     updateTriangle.innerText = totalArea
-
-//     console.log(input1.value)
-//     console.log(input2.value)
-// })
-
-
 
 valueCheck('inb','tri-b')
 valueCheck('inh','tri-h')
+valueCheck('inw','rec-w')
+valueCheck('ini','rec-i')
+
+
+let  i = 0;
 
 document.getElementById('calculate-traingle').addEventListener('click',function(){
 
     let updateB = updateValue('inb','tri-b')
     let updateH = updateValue('inh','tri-h')
+    const totalTriArea = 0.5 * updateB * updateH
+    console.log(updateB,updateH)
+    if(totalTriArea)
+    {
+        const newHtml = document.getElementById('result')
+        const div = document.createElement('div')
+       
+        div.classList.add('flex', 'justify-evenly', 'items-center', 'pt-7')
+       
+        div.innerHTML = `
+        <p><span>${++i}. </span>Triangle</p>
+        <p><span id="tri-value">${totalTriArea}</span> cm<sup>2</sup></p>
+        <button class="bg-[#1090D8] text-sm px-2 text-white rounded-lg py-2  font-semibold">Convert to m<sup>2</sup></button>
+        `
+        newHtml.appendChild(div)
+        const updateTri = document.getElementById('tri-value')
+        updateTri.innerText = totalTriArea
+    }
+    else{
+        alert('please enter the number')
+    }
 
-    const totalArea = 0.5 * updateB * updateH
+})
 
-    const updateTriangle = document.getElementById('triangle-value')
-    updateTriangle.innerText = totalArea
+document.getElementById('calculate-rec').addEventListener('click',function(){
+    let updatew = updateValue('inw','rec-w')
+    let updatei = updateValue('ini','rec-i')
+    const totalRecArea = updatew * updatei
+
+ if(totalRecArea){
+    const newHtml = document.getElementById('result')
+    const div = document.createElement('div')
+   
+    div.classList.add('flex', 'justify-evenly', 'items-center', 'pt-7')
+   
+    div.innerHTML = `
+    <p><span>${++i}. </span>Rectengle</p>
+    <p><span id="rec-value">${totalRecArea}</span> cm<sup>2</sup></p>
+    <button class="bg-[#1090D8] text-sm px-2 text-white rounded-lg py-2  font-semibold">Convert to m<sup>2</sup></button>
+    `
+    newHtml.appendChild(div)
+    const updateRec = document.getElementById('rec-value')
+    updateRec.innerText = totalRecArea
+ }
+ else{
+    alert('please enter the number')
+ }
 })
--- JS/common.js
+++ JS/common.js
@@ -29,3 +29,26 @@ function updateValue(inputId,elementId){
     return boxValue;
    
 }
+
+// document.getElementById('calculate-traingle').addEventListener('click',function(){
+//     const input1 = document.getElementById('inb')
+//     const input2 = document.getElementById('inh')
+
+//     const updateTrih = document.getElementById('tri-h')
+//     const updateTrib = document.getElementById('tri-b')
+
+//     const updateTriangle = document.getElementById('triangle-value')
+
+
+//     updateTrib.innerText = input1.value
+//     updateTrih.innerText = input2.value
+
+//     const totalArea = 0.5 * input1.value * input2.value
+
+//     updateTriangle.innerText = totalArea
+
+//     console.log(input1.value)
+//     console.log(input2.value)
+// })
+
+
